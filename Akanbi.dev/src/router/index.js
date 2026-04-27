d4094de6a5209e7ac9ceb80e1937a989e1652142
ibmoore04 <akanbiibrahimolatunde27@gmail.com>
2026-04-27 10:01:43 +0100

    updated

--- Akanbi.dev/src/router/index.js
+++ Akanbi.dev/src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
-  history: createWebHistory('/Akanbi.dev/'), // 👈 PUT YOUR REPO NAME HERE
+  history: createWebHistory('/'), // 👈 PUT YOUR REPO NAME HERE
   routes: [
     {
       path: '/',
--- Akanbi.dev/vite.config.js
+++ Akanbi.dev/vite.config.js
@@ -4,7 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/Akanbi.dev/',
+  base: '/', // ✅ FIXED for Vercel
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
